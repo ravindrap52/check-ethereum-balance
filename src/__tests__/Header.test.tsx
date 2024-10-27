aca3524f7ecc0451adb3079a6eb9e51818ebeef6
ravindrap52 <ravindrapallikonda@gmail.com>
2024-10-27 15:49:01 +0100

    test(unit-tests): updated unit tests

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -4,13 +4,19 @@ import { describe, it, expect } from 'vitest';
 
 import tatumLogo from '../assets/tatum.jpeg';
 import { Header } from '@/components/Header';
+import { I18nContext } from '@/context/i18nContext';
 
+const MyComponent = () => (
+  <I18nContext.Provider value={{ locale: 'en', setLocale: () => {} }}>
+    <Header />
+  </I18nContext.Provider>
+);
 describe('Header', () => {
   it('should render header', () => {
-    render(<Header />);
+    render(<MyComponent />);
   });
   it('should render header with image', () => {
-    const { getByTestId } = render(<Header />);
+    const { getByTestId } = render(<MyComponent />);
     expect(getByTestId('header')).toBeInTheDocument();
     const imgElement = screen.getByAltText('Tatum');
     expect(imgElement).toHaveAttribute('src', tatumLogo);
